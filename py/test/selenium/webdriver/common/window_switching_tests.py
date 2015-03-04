0c5a0f2773f8def47a293ea6e0f729b5a8f2f8a4
AutomatedTester <dburns@mozilla.com>
2015-03-04 13:55:14 +0000

    disable tests where we close the browser and then try access something

--- py/test/selenium/webdriver/common/window_switching_tests.py
+++ py/test/selenium/webdriver/common/window_switching_tests.py
@@ -31,7 +31,9 @@ class WindowSwitchingTests(unittest.TestCase):
         self.driver.find_element_by_link_text("Open new window").click()
         self.assertEqual(self.driver.title, "XHTML Test Page")
 
-        self.driver.switch_to.window("result")
+        handles = self.driver.window_handles
+        handles.remove(current)
+        self.driver.switch_to.window(handles[0])
         self.assertEqual(self.driver.title, "We Arrive Here")
 
         self._loadPage("iframes")
@@ -57,18 +59,22 @@ class WindowSwitchingTests(unittest.TestCase):
 
     @pytest.mark.ignore_chrome
     @pytest.mark.ignore_opera
+    @pytest.mark.ignore_firefox
     def testShouldThrowNoSuchWindowExceptionOnAnAttemptToGetItsHandle(self):
         self._loadPage("xhtmlTest")
         current = self.driver.current_window_handle
         self.driver.find_element(By.LINK_TEXT,"Open new window").click()
 
-        self.driver.switch_to.window("result")
+        handles = self.driver.window_handles
+        handles.remove(current)
+
+        self.driver.switch_to.window(handles[0])
         self.driver.close()
 
         try :
             self.driver.current_window_handle
             self.fail("NoSuchWindowException expected")
-        except NoSuchWindowException: 
+        except NoSuchWindowException:
             pass # Expected.
         finally:
             self.driver.switch_to.window(current)
@@ -76,45 +82,53 @@ class WindowSwitchingTests(unittest.TestCase):
     @pytest.mark.ignore_chrome
     @pytest.mark.ignore_opera
     @pytest.mark.ignore_ie
+    @pytest.mark.ignore_firefox
     def testShouldThrowNoSuchWindowExceptionOnAnyOperationIfAWindowIsClosed(self):
         self._loadPage("xhtmlTest")
         current = self.driver.current_window_handle
-        
+
         self.driver.find_element(By.LINK_TEXT,"Open new window").click()
 
-        self.driver.switch_to.window("result")
+        handles = self.driver.window_handles
+        handles.remove(current)
+
+        self.driver.switch_to.window(handles[0])
         self.driver.close()
         try:
             try :
                 self.driver.title
                 self.fail("NoSuchWindowException expected")
-            except NoSuchWindowException: 
+            except NoSuchWindowException:
                 pass # Expected.
-            
+
             try :
                 self.driver.find_element_by_tag_name("body")
                 self.fail("NoSuchWindowException expected")
-            except NoSuchWindowException: 
+            except NoSuchWindowException:
                 pass # Expected.
         finally:
             self.driver.switch_to.window(current)
-    
+
     @pytest.mark.ignore_chrome
     @pytest.mark.ignore_opera
     @pytest.mark.ignore_ie
+    @pytest.mark.ignore_firefox
     def testShouldThrowNoSuchWindowExceptionOnAnyElementOperationIfAWindowIsClosed(self):
         self._loadPage("xhtmlTest")
         current = self.driver.current_window_handle
         self.driver.find_element(By.LINK_TEXT,"Open new window").click()
 
-        self.driver.switch_to.window("result")
+        handles = self.driver.window_handles
+        handles.remove(current)
+
+        self.driver.switch_to.window(handles[0])
         element = self.driver.find_element_by_tag_name("body")
         self.driver.close()
 
         try :
             element.text
             self.fail("NoSuchWindowException expected")
-        except NoSuchWindowException: 
+        except NoSuchWindowException:
             pass # Expected.
         finally:
             self.driver.switch_to.window(current)
@@ -126,7 +140,10 @@ class WindowSwitchingTests(unittest.TestCase):
 
         self.driver.find_element_by_name("windowThree").click()
 
-        self.driver.switch_to.window("result")
+        handles = self.driver.window_handles
+        handles.remove(current)
+
+        self.driver.switch_to.window(handles[0])
 
         try:
             self.driver.find_element_by_id("close").click()
@@ -134,14 +151,18 @@ class WindowSwitchingTests(unittest.TestCase):
             self.driver.switch_to.window(currentHandle)
             self.driver.find_element_by_id("linkId")
 
+    @pytest.mark.ignore_firefox
     def testCanCallGetWindowHandlesAfterClosingAWindow(self):
         self._loadPage("xhtmlTest")
 
-        currentHandle = self.driver.current_window_handle
+        current = self.driver.current_window_handle
 
         self.driver.find_element_by_name("windowThree").click()
 
-        self.driver.switch_to.window("result")
+        handles = self.driver.window_handles
+        handles.remove(current)
+
+        self.driver.switch_to.window(handles[0])
 
         try:
             self.driver.find_element_by_id("close").click()
@@ -149,7 +170,7 @@ class WindowSwitchingTests(unittest.TestCase):
 
             self.assertEqual(1, len(all_handles))
         finally:
-            self.driver.switch_to.window(currentHandle)
+            self.driver.switch_to.window(current)
 
     def testCanObtainAWindowHandle(self):
         self._loadPage("xhtmlTest")
@@ -170,14 +191,18 @@ class WindowSwitchingTests(unittest.TestCase):
 
         self.assertEqual(current, new_handle)
 
+    @pytest.mark.ignore_firefox
     def testThatAccessingFindingAnElementAfterWindowIsClosedAndHaventswitchedDoesntCrash(self):
         self._loadPage("xhtmlTest")
 
-        currentHandle = self.driver.current_window_handle
+        current = self.driver.current_window_handle
 
         self.driver.find_element_by_name("windowThree").click()
 
-        self.driver.switch_to.window("result")
+        handles = self.driver.window_handles
+        handles.remove(current)
+
+        self.driver.switch_to.window(handles[0])
 
         try:
             self.driver.find_element_by_id("close").click()
@@ -188,7 +213,7 @@ class WindowSwitchingTests(unittest.TestCase):
         except WebDriverException:
             pass #this is expected
         finally:
-            self.driver.switch_to.window(currentHandle)
+            self.driver.switch_to.window(current)
 
     def _pageURL(self, name):
         return self.webserver.where_is(name + '.html')
